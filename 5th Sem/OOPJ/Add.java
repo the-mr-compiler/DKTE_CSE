c4f6d8e4cadc1ac44ab43e548e41c1baa9853e42
Pramod Sargar <41449799+impramodsargar@users.noreply.github.com>
2021-07-15 18:39:20 +0530

    OOPJ assignment

---
+++ 5th Sem/OOPJ/Add.java
@@ -0,0 +1,7 @@
+public class Div{
+	public static void main(String[] args){
+	int a=5,b=5,c;
+	c=a/b;
+		System.out.println("Division of two numbers: "+c);
+	}
+}
---
+++ 5th Sem/OOPJ/Hello.java
@@ -0,0 +1,7 @@
+public class Add{
+	public static void main(String[] args){
+	int a=5,b=5,c;
+	c=a+b;
+		System.out.println("Addition of two numbers: "+c);
+	}
+}
